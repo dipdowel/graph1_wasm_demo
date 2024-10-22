0159b06b701d191d8b3fbc8d091322df7c79bf2a
dipdowel <leonid.bogouslavski@gmail.com>
2024-10-22 22:41:24 +0200

    - PoC: State of the WASM module can now be initialized and then updated

--- src/lib.rs
+++ src/lib.rs
@@ -3,21 +3,62 @@ mod web_utils;
 use wasm_bindgen::prelude::*;
 use crate::web_utils::console_log;
 
-static mut NAMES: Option<Vec<String>> = None;
-
-
-/// WASM module entry point
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    // Use an unsafe block only where accessing the static mutable variable
-    unsafe {
-        if NAMES.is_none() {
-            NAMES = Some(Vec::new());
-        }
-        NAMES.as_mut().unwrap().push(name.to_string());
-    }
-
-    // Safely log the names outside the unsafe block
-    let all_names = unsafe { NAMES.as_ref().unwrap().join(", ") };
-    console_log(&format!("Hello, {} there!  Previous names: {}", name, all_names));
+struct AppState {
+    frame_count:usize,
+    name:String,
 }
+
+impl AppState {
+    fn new(name:String, frame_count:usize) -> Self {
+        AppState{
+            name,
+            frame_count
+        }
+    }
+}
+
+static mut STATE: Option<AppState> = None;
+
+
+
+#[wasm_bindgen]
+/// Initialize the WASM module: create `STATE` and populate it with the initial values
+pub fn init_module(name: &str, frame:usize) {
+    unsafe {
+        if STATE.is_none() {
+            STATE = Some(AppState::new(name.to_string(), frame));
+        }
+    }
+}
+
+
+#[wasm_bindgen]
+/// Set current frame (if STATE exists)
+pub fn update_frame(frame: usize) {
+    unsafe {
+        // If STATE is Some, update the frame_count with the provided frame value
+        if let Some(state) = STATE.as_mut() {
+            state.frame_count = frame;
+            console_log(&format!(
+                "[ {} ] Current frame: {}",
+                state.name, state.frame_count
+            ));
+        }
+    }
+}
+
+#[wasm_bindgen]
+/// Set name (if STATE exists)
+pub fn set_name(name: String) {
+    unsafe {
+        // If STATE is Some, update the name with the provided value
+        if let Some(state) = STATE.as_mut() {
+            state.name = name.clone();
+            console_log(&format!(
+                "Name updated to: {}! Current frame: {}",
+                state.name, state.frame_count
+            ));
+        }
+    }
+}
+
