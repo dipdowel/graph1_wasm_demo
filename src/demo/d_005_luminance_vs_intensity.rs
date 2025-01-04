12df156e11f72dabcb3ffa42a143b0978bf89ae9
dipdowel <leonid.bogouslavski@gmail.com>
2025-01-04 01:41:25 +0100

    - a buffer overflow bug fixed

--- src/demo/d_005_luminance_vs_intensity.rs
+++ src/demo/d_005_luminance_vs_intensity.rs
@@ -178,7 +178,7 @@ pub fn render_frame(ctx: &mut GraphContext<DemoUserData>) {
     draw::tools::fill::buffer(&mut ctx.frame_buf, 0xff_00_ff_ff, ctx.num_threads);
 
     // Change direction when the ghost approaches the edge of the screen
-    if current_frame % (ctx.win.w_i32 - 66) == 0 {
+    if current_frame % (ctx.win.w_i32 - 76) == 0 {
         ctx.user_data.ghosts.direction.x *= -1;
     }
 
