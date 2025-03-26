f3e4dcb67a9d8b9524c714b36b0bd382f099e93a
dipdowel <leonid.bogouslavski@gmail.com>
2025-03-26 03:30:51 +0100

    - Working on the shapes

--- src/demo/d_007_shapes.rs
+++ src/demo/d_007_shapes.rs
@@ -1,14 +1,16 @@
 use crate::demo::user_data::DemoUserData;
 use graph1::core::context::GraphContext;
+use graph1::core::default_colors::BACKGROUND;
 use graph1::draw;
-use graph1::fx::{glitch, scanline};
 use graph1::fx::glitch::HorizontalGlitchProps;
 use graph1::fx::scanline::window;
-use graph1::primitives::Pixel;
-use graph1::primitives::plane::RectArea;
+use graph1::fx::{glitch, scanline};
+use graph1::primitives::plane::{Dimensions2d, RectArea};
 use graph1::primitives::point::Point;
+use graph1::primitives::Pixel;
 use graph1::utils::clear_screen;
 use graph1::utils::color::palettes::RetroNeon;
+use graph1::utils::color::{gradient, invert};
 use graph1::utils::math::oscillator;
 
 //---------------------------------------------------------------------
@@ -17,17 +19,15 @@ use graph1::utils::math::oscillator;
 //     pub direction: Point<i32>,
 //     pub current_point: Point<i32>,
 // }
-// 
+//
 // pub const SHAPES_USER_DATA: ShapesUserData = ShapesUserData {
 //     direction: Point { x: 0, y: 0 },
 //     current_point: Point { x: 0, y: 0 },
 // };
 //---------------------------------------------------------------------
- 
+
 /// Illustrates how color Intensity and color Luminance are different
 pub fn render_frame(ctx: &mut GraphContext<DemoUserData>) {
-
-
     // initialize the data maintained between frames
     // if current_frame == 0 {
     //     ctx.user_data.ghosts.current_point.x = 26;
@@ -37,32 +37,78 @@ pub fn render_frame(ctx: &mut GraphContext<DemoUserData>) {
     //     return;
     // }
 
+    if ctx.frame_count == 0 {
+        ctx.win.background_color = 0x33333300;
+        // ctx.win.background_color = 0x000000ff   ;
+    }
+
     clear_screen(ctx);
 
-    let c:(Pixel, Pixel, Pixel, Pixel) = (
-        ctx.win.quadrants.top_left.center().to_pixel(RetroNeon::CIRCUIT_GREEN),
-        ctx.win.quadrants.top_right.center().to_pixel(RetroNeon::ACID_GREEN),
-        ctx.win.quadrants.bottom_left.center().to_pixel(RetroNeon::LASER_LIME),
-        ctx.win.quadrants.bottom_right.center().to_pixel(RetroNeon::CYBER_YELLOW),
+    let max_radius = ctx.win.quadrants.bottom_left.width() / 4;
+
+    let r: (u32, u32, u32, u32) = (
+        oscillator::sine(ctx.frame_count + 0, 0.015, 4, max_radius) as u32,
+        oscillator::sine(ctx.frame_count + 40, 0.015, 4, max_radius) as u32,
+        oscillator::sine(ctx.frame_count + 80, 0.015, 4, max_radius) as u32,
+        oscillator::sine(ctx.frame_count + 120, 0.015, 4, max_radius) as u32,
+    );
+    let bg = RetroNeon::DEEP_SPACE_BLUE;
+
+    // let fg:Vec<u32> = gradient::linear(RetroNeon::CYBER_BLUE, RetroNeon::NEON_PINK, 14);
+
+    let fg = [
+        gradient::linear(RetroNeon::ELECTRIC_BLUE, RetroNeon::CYBER_YELLOW, 8),
+        gradient::linear(RetroNeon::CYBER_YELLOW, RetroNeon::ELECTRIC_BLUE, 8),
+    ]
+    .concat();
+
+    let mut i: usize =
+        oscillator::sine(ctx.frame_count, 0.000055 * max_radius as f64, 0, 7) as usize;
+    i = i.min(i - 4);
+
+    let color: (u32, u32, u32, u32) = (
+        gradient::linear_step(bg, fg[i], max_radius as usize, r.0 as usize),
+        gradient::linear_step(bg, fg[i + 1], max_radius as usize, r.1 as usize),
+        gradient::linear_step(bg, fg[i + 2], max_radius as usize, r.2 as usize),
+        gradient::linear_step(bg, fg[i + 3], max_radius as usize, r.3 as usize),
     );
 
-    let max_radius = ctx.win.quadrants.bottom_left.width()/4;
+    let center: (Pixel, Pixel, Pixel, Pixel) = (
+        ctx.win.quadrants.top_left.center().to_pixel(color.0),
+        ctx.win.quadrants.top_right.center().to_pixel(color.1),
+        ctx.win.quadrants.bottom_left.center().to_pixel(color.2),
+        ctx.win.quadrants.bottom_right.center().to_pixel(color.3),
+    );
 
-    draw::circle::filled(ctx, &c.3, oscillator::sine(ctx.frame_count + 0, 0.02, 4, max_radius) as u32, 0);
-    draw::circle::filled(ctx, &c.1, oscillator::sine(ctx.frame_count + 35, 0.02, 4, max_radius)  as u32, 0);
-    draw::circle::filled(ctx, &c.2, oscillator::sine(ctx.frame_count  + 70, 0.02, 4, max_radius) as u32, 0);
-    draw::circle::filled(ctx, &c.0, oscillator::sine(ctx.frame_count + 105, 0.02, 4, max_radius) as u32, 0);
+    draw::circle::filled(ctx, &center.3, r.0, 0);
+    draw::circle::filled(ctx, &center.1, r.1, 0);
+    draw::circle::filled(ctx, &center.2, r.2, 0);
+    draw::circle::filled(ctx, &center.0, r.3, 0);
+
+    draw::circle::filled(
+        ctx,
+        &ctx.win.center.to_pixel(RetroNeon::STROBE_WHITE),
+        max_radius * 100 / 45,
+        4,
+    );
 
 
+    draw::rectangle::filled(ctx, &RectArea {
+        top_left: ctx.win.quadrants.top_left.right() ,
+        dimensions: Dimensions2d::new(100, 100),
+        color: Some(RetroNeon::GLITCH_RED),
+    });
+        
+        
 
-    // glitch::horizontal_glitch(
+    // invert::invert_colors(
     //     ctx,
-    //     &mut HorizontalGlitchProps {
-    //         strength: 30,
-    //         chance: 10,
-    //         left_right_balance: 128,
+    //     &RectArea {
+    //         top_left: ctx.win.quadrants.top_left.center(),
+    //         dimensions: ctx.win.quadrants.top_left.rect_area().dimensions,
+    //         color: None,
     //     },
-    //     None,
     // );
 
+    // scanline::window(ctx, 1, 0x25);
 }
