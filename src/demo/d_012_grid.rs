a85e39f6ed4439542d85baea864e7fd7f21db555
dipdowel <leonid.bogouslavski@gmail.com>
2025-05-16 16:41:12 +0200

    - grid demo, first test

---
+++ src/demo/d_012_grid.rs
@@ -0,0 +1,97 @@
+use crate::demo::user_data::DemoUserData;
+use graph1::core::context::GraphContext;
+use graph1::draw;
+
+use graph1::fx::scanline;
+use graph1::utils::clear_screen;
+use graph1::utils::color::palettes::RetroNeon;
+
+use graph1::draw::curve::bezier_segment::BezierSegment;
+use graph1::draw::polygons::{polygon, star, PolygonProperties, StarProperties};
+use graph1::draw::tools::fill;
+use graph1::primitives::point::Point;
+use graph1::primitives::Pixel;
+use graph1::primitives::plane::{Dimensions2d, RectArea};
+use graph1::text::font::{PixelFont, Spacing};
+use graph1::text::font_embedder::{instantiate_embedded_font, EmbeddedFonts};
+use graph1::text::printer;
+use graph1::utils::color::gradient;
+use graph1::utils::grid::uniform::UniformGrid;
+use graph1::utils::math::oscillator;
+
+//---------------------------------------------------------------------
+// Configure the user data for typing text in Basic Concepts pt. 1
+pub struct BezierCurvesUserData {
+    pub text_color_props: printer::ColorProperties<'static>,
+    pub text_font: Option<PixelFont>,
+}
+
+pub const BEZIER_CURVES_USER_DATA: BezierCurvesUserData = BezierCurvesUserData {
+    text_color_props: printer::ColorProperties {
+        color: Some(RetroNeon::STROBE_WHITE),
+        color_transformer: None,
+        data: None,
+    },
+    // Let's put an instantiated font into the user data
+    // so that we don't have to instantiate it on every frame
+    text_font: None,
+};
+//---------------------------------------------------------------------
+
+pub fn render_frame(ctx: &mut GraphContext<DemoUserData>) {
+    // initial context setup
+    if ctx.frame_count == 0 {
+ 
+    }
+
+    clear_screen(ctx);
+
+
+    let mut grid = UniformGrid::new(RectArea::new(
+        0,
+        0,
+        30,
+        16,
+        Some(ctx.win.background_color),
+    ),
+        
+        8,
+        8,
+     );
+
+
+    let mut colors = gradient::linear(
+        ctx.win.background_color,
+        ctx.win.foreground_color,
+        grid.num_cells()+10, 
+    );
+    
+    grid.iter().for_each(|cell| {
+        // let color = colors.pop();
+        // ctx.win.foreground_color = color.unwrap();
+        draw::rectangle::filled(
+            ctx, 
+            &RectArea{
+                top_left: Point::new(cell.center().x, cell.center().y),
+                dimensions: Dimensions2d::new(10,6),
+                color: Some(ctx.win.foreground_color),
+            }
+        );
+    })
+    
+    // 
+    // for i in 0..grid.num_cells() {
+    //     let color = colors[i];
+    //     draw::rectangle::filled(
+    //         ctx, 
+    //         &RectArea::new(
+    //             grid.cell(i).x,
+    //             grid.cell(i).y,
+    //             grid.cell(i).w,
+    //             grid.cell(i).h,
+    //             Some(color),
+    //         ),
+    //  
+    // 
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ mod utils;
 
 use crate::demo::user_data::DemoUserData;
 
-use crate::demo::{d_000_intro, d_001_basic_concepts_pt1, d_002_basic_concepts_pt2, d_003_basic_concepts_pt3, d_004_bouncy, d_005_alpha, d_006_luminance_vs_intensity, d_007_shapes_and_quadrants, d_008_polygons, d_009_lines, d_010_quadrants, d_011_curves};
+use crate::demo::{d_000_intro, d_001_basic_concepts_pt1, d_002_basic_concepts_pt2, d_003_basic_concepts_pt3, d_004_bouncy, d_005_alpha, d_006_luminance_vs_intensity, d_007_shapes_and_quadrants, d_008_polygons, d_009_lines, d_010_quadrants, d_011_curves, d_012_grid};
 use crate::utils::console_log;
 use graph1::core::context::{GraphContext, WindowContext};
 use graph1::utils::color;
@@ -34,6 +34,7 @@ pub mod demo {
     pub mod d_009_lines;
     pub mod d_010_quadrants;
     pub mod d_011_curves;
+    pub mod d_012_grid;
 
 
     /// User data, used to store arbitrary data that needs to be persisted between frames
@@ -187,6 +188,7 @@ pub fn update_frame(frame: usize) -> PixelStats {
                 9 => d_009_lines::render_frame(&mut ctx),
                 10 => d_010_quadrants::render_frame(&mut ctx),
                 11 => d_011_curves::render_frame(&mut ctx),
+                12 => d_012_grid::render_frame(&mut ctx),
 
                 // 5 => circles::render_frame(&mut ctx),
                 _ => d_011_curves::render_frame(&mut ctx),
