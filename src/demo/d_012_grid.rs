511e198094f8093b16aabee07178597a3d8dbf6b
dipdowel <leonid.bogouslavski@gmail.com>
2025-05-29 11:48:17 +0200

    Uniform grid demo - Some code clean-up

--- src/demo/d_012_grid.rs
+++ src/demo/d_012_grid.rs
@@ -1,26 +1,19 @@
 use crate::demo::user_data::DemoUserData;
 use graph1::core::context::GraphContext;
-use graph1::{draw, hash_random_u32};
+use graph1::draw;
 
 use graph1::fx::{glitch, scanline};
 use graph1::utils::color::palettes::{OceanBreeze, RetroNeon};
-use graph1::utils::{clear_screen, grid};
+use graph1::utils::clear_screen;
 
-use graph1::draw::curve::bezier_segment::BezierSegment;
-use graph1::draw::polygons::{closed_perimeter, polygon, star, PolygonProperties, StarProperties};
-use graph1::draw::tools::fill;
-use graph1::primitives::neighborhood::NeighborhoodType;
-use graph1::primitives::numeric::Numeric;
-use graph1::primitives::plane::{Dimensions2d, RectArea};
-use graph1::primitives::point::{Point, POINT_ONE};
-use graph1::primitives::Pixel;
-use graph1::text::font::{PixelFont, Spacing};
-use graph1::text::font_embedder::{instantiate_embedded_font, EmbeddedFonts};
+use graph1::draw::polygons::closed_perimeter;
+use graph1::primitives::plane::RectArea;
+use graph1::primitives::point::Point;
+use graph1::text::font::PixelFont;
 use graph1::text::printer;
-use graph1::utils::color::{alpha, gradient};
+use graph1::utils::color::gradient;
 
 use graph1::utils::grid::uniform::UniformGrid;
-use graph1::utils::math::geometry::region::Region;
 use graph1::utils::math::oscillator;
 
 use graph1::draw::rectangle::filled as draw_rect;
@@ -107,18 +100,18 @@ pub fn bar_3d<UserData>(ctx: &mut GraphContext<UserData>, props: &Bar3DProps) {
         draw::line::horizontal(ctx, &start, width, Some(color_top));
     }
 
-    // SIDE face (slanted parallelogram)
+    // RIGHT-SIDE face (slanted parallelogram)
     let side = vec![
         Point::new((x + width) as i32, (y - height) as i32),
         Point::new((x + depth + width) as i32, (y - height - depth) as i32),
         Point::new((x + depth + width) as i32, (y - depth) as i32),
         Point::new((x + width) as i32, y as i32),
-    ];
+    ];    
     closed_perimeter(ctx, &side, Some(color_side));
+    
     flood(
         &mut ctx.frame_buf,
         &ctx.win.dimensions,
-        // &(side[0] + Point::new(2, 2)).to_pixel(color_side),
         &(side[0] + Point::new(2, 0)).to_pixel(color_side),
     );
 }
@@ -154,11 +147,9 @@ pub fn render_frame(ctx: &mut GraphContext<DemoUserData>) {
         num_rows = 3;
     }
 
-
     let cell_width: u32 = ctx.win.w / num_cols;
     let cell_height: u32 = ctx.win.h / num_rows;
     ctx.line.set_int_no_aa(Some(2));
-
 
     let mut grid = UniformGrid::new(
         RectArea::new(0, 0, cell_width, cell_height, Some(RetroNeon::LASER_AQUA)),
@@ -169,19 +160,10 @@ pub fn render_frame(ctx: &mut GraphContext<DemoUserData>) {
         ]),
     );
 
-    //==============================================================================================
-    //==============================================================================================
-
-    // let w_osc = oscillator::sine(ctx.frame_count, 0.005, cell_width, cell_width*10/8) as u32;
-
-    // grid.resize_cells(Dimensions2d::new(w_osc, cell_height));
-
-    // grid.resize_grid(num_rows as usize *2, num_cols as usize, None );
-
+    //----------------------------------------------------------------------------------------------
+    // Let's use the bottom-left corner of the first cell as the starting point for the 3D bars
     let origin = grid.get_cell(0, 0).unwrap().bottom_left();
-
-    //
-    // Set up the first 3D bar
+    // Reusable configuration for the 3D bar
     let mut props = Bar3DProps {
         x: origin.x,
         y: origin.y,
@@ -194,9 +176,14 @@ pub fn render_frame(ctx: &mut GraphContext<DemoUserData>) {
     };
     //
 
+    // Max height for the bars, relative to the grid cell height
     let height_bound = (cell_height as f32 * 0.78) as u32;
 
-    // Generate oscillating values for animating the bar heights
+    //
+    //----------------------------------------------------------------------------------------------
+    // Generate oscillated values for animating the bar heights
+    // The frequency is slightly varied to create a wave-like effect,
+    // yet irregular enough to be entertaining.
     let mut osc = [
         oscillator::sine(ctx.frame_count, 0.0060, 4, height_bound) as u32,
         oscillator::sine(ctx.frame_count, 0.0065, 4, height_bound) as u32,
@@ -207,95 +194,25 @@ pub fn render_frame(ctx: &mut GraphContext<DemoUserData>) {
         oscillator::sine(ctx.frame_count, 0.0090, 4, height_bound) as u32,
     ];
 
-
+    //
+    //----------------------------------------------------------------------------------------------
     // Iterate over the grid cells and draw a 3D bar per each cell
-    grid.iter().enumerate().for_each(|(i,cell)| {
+    grid.iter().enumerate().for_each(|(i, cell)| {
+        // Let's use the bottom-left corner of each cell as the base point for the current 3D bar
         let cell_point = cell.bottom_left();
         props.x = cell_point.x;
         props.y = cell_point.y;
-        props.height = osc[ i % osc.len()];
+        // Let's pick up the height from the oscillated values, using the index to rotate through them
+        props.height = osc[i % osc.len()];
         bar_3d(ctx, &props);
+
+        // An extra offset to make each row look 1 step different from the previous one
         osc.rotate_left(1);
     });
 
-
-
-
-    // GRID RENDERING
-    // grid::uniform::render(ctx, &grid, true);
-
     //
-    // for c in 0..num_cols {
-    //     bar_3d(ctx, &props);
-    //     props.x += cell_width;
-    // }
-    // props.y += cell_height;
-    // props.x = 0;
-    // for c in 0..num_cols {
-    //     bar_3d(ctx, &props);
-    //     props.x += cell_width;
-    // }
-
-    //
-    // props.x += 21;
-    // props.height -= 24;
-    // bar_3d(ctx, &props);
-    //
-
-    //==============================================================================================
-    //==============================================================================================
-
-    // // let neighborhood = NeighborhoodType::Circle {radius: 16};
-    // // let neighborhood = NeighborhoodType::Diamond {distance: 12};
-    // // let neighborhood = NeighborhoodType::Diagonal;
-    // // let neighborhood = NeighborhoodType::Immediate;
-    // // let neighborhood = NeighborhoodType::Orthogonal;
-    // let neighborhood = NeighborhoodType::Square {distance: 4};
-    //
-    // let neighbors = grid.get_neighbors( 18,25, &neighborhood);
-    //
-    // ctx.line.set_int_no_aa(Some(1));
-    //
-    // neighbors.iter().for_each(|cell| {
-    //
-    //     // fill::flood(&mut ctx.frame_buf, &ctx.win.dimensions, &cell.cell.center().to_pixel(RetroNeon::LASER_LIME));
-    //     let mut cell_area = cell.cell.rect_area();
-    //     cell_area.color = Some(RetroNeon::LASER_LIME);
-    //     draw::rectangle::filled(ctx, &cell_area);
-    // });
-
-    // #############################################################################################
-
-    // println!("\n\nsurrounding: {:?}", surrounding);
-
-    // grid.iter().for_each(|cell| {
-    //
-    //     let p:Point;
-    //
-    //
-    //     match ctx.frame_count % 80 {
-    //         0..=10 => { p=cell.top(); },
-    //         11..=20 => { p=cell.top_right(); },
-    //         21..=30 => { p=cell.right(); },
-    //         31..=40 => { p=cell.bottom_right(); },
-    //         41..=50 => { p=cell.bottom(); },
-    //         51..=60 => { p=cell.bottom_left(); },
-    //         61..=70 => { p=cell.left(); },
-    //         71..=80 => { p=cell.top_left(); },
-    //         // 81..=90 => { p=cell.center(); },
-    //         _ =>   p=cell.center(), // defensive: % 8 guarantees 0–7
-    //     }
-    //
-    //
-    //     draw::rectangle::filled(ctx,
-    //         &RectArea {
-    //             top_left: Point::new(p.x , p.y ),
-    //             dimensions: Dimensions2d::square(ctx.win.h/20),
-    //             color: Some( alpha::set_alpha(RetroNeon::STROBE_WHITE, 190)),
-    //         }
-    //     )
-    // });
-
+    //----------------------------------------------------------------------------------------------
+    // Transition between scenes of the demo
     let is_transition = (ctx.frame_count % 1800 > 585 && ctx.frame_count % 1800 < 615)
         || (ctx.frame_count % 1800 > 1185 && ctx.frame_count % 1800 < 1215)
         || (ctx.frame_count % 1800 > 1785 || ctx.frame_count % 1800 < 15);
@@ -310,22 +227,9 @@ pub fn render_frame(ctx: &mut GraphContext<DemoUserData>) {
             },
             None,
         );
-
     }
 
-
-    // let os: u32 = oscillator::sine(50 + ctx.frame_count, 0.0005, 0, 200) as u32;
-    // let is_glitching = os > 195 || os < 5 || (os > 90 && os < 95) || (os > 41 && os < 45);
-    // if is_glitching {
-    // glitch::horizontal_glitch(
-    //     ctx,
-    //     &mut HorizontalGlitchProps {
-    //         strength: 4,
-    //         chance: 40,
-    //         left_right_balance: 128,
-    //     },
-    //     None,
-    // );}
-
+    //----------------------------------------------------------------------------------------------
+    // The usual scanline effect. Classic stuff! 😌
     scanline::window(ctx, 1, 66);
 }
