1a3dcde3a073cc3c42219f4b26a048a0608dc1ad
dipdowel <leonid.bogouslavski@gmail.com>
2025-06-12 22:07:35 +0200

    The cool green spray pattern fine tuned

--- src/demo/d_014_brushes.rs
+++ src/demo/d_014_brushes.rs
@@ -1,6 +1,6 @@
-use std::ptr;
 use crate::demo::user_data::DemoUserData;
-use graph1::core::context::{FrameBuffer,  GraphContext};
+use graph1::core::context::{FrameBuffer, GraphContext};
+use std::ptr;
 
 use graph1::utils::clear_screen;
 use graph1::utils::color::palettes::RetroNeon;
@@ -10,6 +10,9 @@ use graph1::primitives::{plane::RectArea, point::Point};
 use graph1::utils::grid::uniform::UniformGrid;
 use graph1::utils::math::oscillator;
 
+use crate::demo::elements::d_014_spray_config::{
+    BlendSettings, BrushSettings, PathBounds, SprayDemoSceneConfig,
+};
 use graph1::core::context_utils::line_clipping_style::LineClippingStyle;
 use graph1::draw::rectangle;
 use graph1::draw::tools::brush::Brush;
@@ -19,37 +22,32 @@ use graph1::primitives::math::{Bound, Shell};
 use graph1::primitives::plane::Dimensions2d;
 use graph1::utils::color::gradient;
 use graph1::utils::color::math::{rgba_operation, ColorOperation};
-use crate::demo::elements::d_014_spray_config::{BlendSettings, BrushSettings, PathBounds, SprayDemoSceneConfig};
 
 //---------------------------------------------------------------------
 // Configure the user data for typing text in Basic Concepts pt. 1
 pub struct BrushUserData {
-    pub  scene_configs:   Vec<SprayDemoSceneConfig>
+    pub scene_configs: Vec<SprayDemoSceneConfig>,
 }
 
-
 pub fn get_brush_user_data() -> BrushUserData {
-
-
     let greens = gradient::linear(RetroNeon::MATRIX_GREEN, 0x00_00_00_ff, 255);
 
     BrushUserData {
-        scene_configs:  vec![
+        scene_configs: vec![
             //
             // ###[ INFINITY ]######################################################################
             //
             SprayDemoSceneConfig {
-                grid_num_cols:10,
-                grid_num_rows:5,
-
+                grid_num_cols: 10,
+                grid_num_rows: 5,
 
                 // Background color of the entire scene
                 background_color: RetroNeon::DEEP_SPACE_BLUE,
 
                 // Outer and inner grid line colors
                 grid_colors: Shell {
-                    outer: RetroNeon::ELECTRIC_BLUE,     // grid_outer_color
-                    inner: RetroNeon::DIGITAL_GOLD,      // grid_inner_color
+                    outer: RetroNeon::ELECTRIC_BLUE, // grid_outer_color
+                    inner: RetroNeon::DIGITAL_GOLD,  // grid_inner_color
                 },
 
                 // Frequencies for X and Y axis distortion
@@ -60,29 +58,28 @@ pub fn get_brush_user_data() -> BrushUserData {
 
                 // Blend modulation frequency and color
                 blend: BlendSettings {
-                    frequency: 4,           // blend_freq
-                    color: 0x04_04_03_ff,   // blend_color
-                    // color: 0x00_04_06_ff,   //TODO: this blend_color is AWESOME! DEFINITELY USE IT!
+                    frequency: 4, // blend_freq
+                    color: 0x04_04_03_ff, // blend_color
+                                  // color: 0x00_04_06_ff,   //TODO: this blend_color is AWESOME! DEFINITELY USE IT!
                 },
 
                 // Outer brush styling
                 outer_brush: BrushSettings {
                     dimensions: Dimensions2d::square(60), // brush_side_outer
-                    density: 130,                          // density_outer
+                    density: 130,                         // density_outer
                     colors: vec![
                         RetroNeon::DEEP_SPACE_BLUE,
-                        RetroNeon::CYBER_BLUE ,
+                        RetroNeon::CYBER_BLUE,
                         RetroNeon::PSYCHEDELIC_BLUE,
                         RetroNeon::ELECTRIC_BLUE,
-                        RetroNeon::PULSING_PURPLE
-
+                        RetroNeon::PULSING_PURPLE,
                     ],
                 },
 
                 // Inner brush styling
                 inner_brush: BrushSettings {
                     dimensions: Dimensions2d::new(42, 12), // brush_side_inner
-                    density: 260, // density_inner
+                    density: 260,                          // density_inner
                     colors: vec![
                         RetroNeon::STROBE_WHITE,
                         RetroNeon::STROBE_WHITE,
@@ -90,7 +87,6 @@ pub fn get_brush_user_data() -> BrushUserData {
                         RetroNeon::CYBER_YELLOW,
                         RetroNeon::STROBE_WHITE,
                         RetroNeon::NEON_ORANGE,
-
                     ],
                 },
 
@@ -98,7 +94,7 @@ pub fn get_brush_user_data() -> BrushUserData {
                 path_bounds: PathBounds {
                     x: Bound {
                         lower: 38, // path_x_lower_bound
-                        upper: 38 // path_x_upper_bound
+                        upper: 38, // path_x_upper_bound
                     },
                     y: {
                         Bound {
@@ -110,7 +106,7 @@ pub fn get_brush_user_data() -> BrushUserData {
 
                 // Grid cell delta sizes for outer and inner squares
                 cell_deltas: Shell {
-                    outer: 4, // cell_outer_delta
+                    outer: 4,  // cell_outer_delta
                     inner: 14, // cell_inner_delta
                 },
             },
@@ -118,18 +114,17 @@ pub fn get_brush_user_data() -> BrushUserData {
             // ###[ GREENISH GLOW ]######################################################################
             //
             SprayDemoSceneConfig {
-
-                grid_num_cols:3,
-                grid_num_rows:4,
+                grid_num_cols: 3,
+                grid_num_rows: 4,
 
                 // Background color of the entire scene
-                background_color: greens[120],
+                background_color: greens[190],
 
                 // Outer and inner grid line colors
                 grid_colors: Shell {
-                    outer: greens[5],     // grid_outer_color
+                    outer: greens[5], // grid_outer_color
                     // inner: greens[35],      // grid_inner_color
-                    inner: RetroNeon::LASER_LIME
+                    inner: RetroNeon::LASER_LIME,
                 },
 
                 // Grid cell delta sizes for outer and inner squares
@@ -138,57 +133,41 @@ pub fn get_brush_user_data() -> BrushUserData {
                     inner: 4, // cell_inner_delta
                 },
 
-                // Frequencies for X and Y axis
-                // frequency: Point {
-                //     x: 0.003, // freq_x
-                //     y: 0.00675, // freq_y
-                // },
-
                 frequency: Point {
-                    x: 0.00402,
+                    x: 0.004,
                     y: 0.0032,
                 },
-
-
 
                 // Outer brush styling
                 outer_brush: BrushSettings {
                     dimensions: Dimensions2d::square(52),
                     density: 44,
                     colors: vec![
-                        // RetroNeon::STROBE_WHITE,
-                        // greens[90],
                         greens[25],
                         greens[50],
                         greens[75],
                         greens[100],
                         greens[125],
                         greens[150],
-                        // greens[14],
-                        // greens[180],
-                        // RetroNeon::STROBE_WHITE,
-                        // greens[20],
-                        // greens[50],
-                        // greens[80],
-                        // greens[110],
-                        // greens[170],
                     ],
                 },
 
                 // Inner brush styling
                 inner_brush: BrushSettings {
-                    dimensions: Dimensions2d::square(12), // brush_side_inner
-                    density: 220, // density_inner
+                    dimensions: Dimensions2d::square(12),
+                    density: 128,
                     colors: vec![
                         RetroNeon::STROBE_WHITE,
-                        RetroNeon::STROBE_WHITE,
+                        RetroNeon::CYBER_YELLOW,
                         RetroNeon::MATRIX_GREEN,
+                        RetroNeon::STROBE_WHITE,
                         RetroNeon::LASER_LIME,
                         RetroNeon::STROBE_WHITE,
                         RetroNeon::STROBE_WHITE,
                         RetroNeon::CYBER_YELLOW,
-                        RetroNeon::LASER_LIME,
                         RetroNeon::STROBE_WHITE,
+                        RetroNeon::LASER_LIME,
+                        RetroNeon::CYBER_YELLOW,
                         RetroNeon::STROBE_WHITE,
                     ],
                 },
@@ -197,7 +176,7 @@ pub fn get_brush_user_data() -> BrushUserData {
                 path_bounds: PathBounds {
                     x: Bound {
                         lower: 21, // path_x_lower_bound
-                        upper: 25 // path_x_upper_bound
+                        upper: 25, // path_x_upper_bound
                     },
                     y: {
                         Bound {
@@ -209,15 +188,12 @@ pub fn get_brush_user_data() -> BrushUserData {
 
                 // Blend modulation frequency and color
                 blend: BlendSettings {
-                    frequency: 3,           // blend_freq
-                    color: 0x01_01_01_ff,   // blend_color
-                    // color: 0x00_04_06_ff,   //TODO: this blend_color is AWESOME! DEFINITELY USE IT!
+                    frequency: 3, // blend_freq
+                    color: 0x01_01_01_ff, // blend_color
+                                  // color: 0x00_04_06_ff,   //TODO: this blend_color is AWESOME! DEFINITELY USE IT!
                 },
-
             },
-
         ],
-
         // brush_context: GraphContext::new(
         //     WindowContext::new(WIN_WIDTH, WIN_HEIGHT, Some(0x00_00_00_ff), Some(0x00_00_00_ff)),
         //     true, false, None, 1, None
@@ -231,28 +207,25 @@ pub fn render_frame(ctx: &mut GraphContext<DemoUserData>) {
     //----------------------------------------------------------------------------------------------
     ctx.set_frame_buf_to(FrameBuffer::Draft);
 
-  /*  // This is a temporary hack to switch between two scenes
-    let mut cfg = ctx.user_data.brush_demo.scene_configs[0].clone();
+    /*  // This is a temporary hack to switch between two scenes
+        let mut cfg = ctx.user_data.brush_demo.scene_configs[0].clone();
 
-    if ctx.frame_count == 1600 {
-        cfg = ctx.user_data.brush_demo.scene_configs[1].clone();
-        ctx.win.background_color = cfg.background_color;
-        clear_screen(ctx);
-    }
+        if ctx.frame_count == 1600 {
+            cfg = ctx.user_data.brush_demo.scene_configs[1].clone();
+            ctx.win.background_color = cfg.background_color;
+            clear_screen(ctx);
+        }
 
-    if ctx.frame_count > 1600 {
-        cfg = ctx.user_data.brush_demo.scene_configs[1].clone();
-    }
-*/
+        if ctx.frame_count > 1600 {
+            cfg = ctx.user_data.brush_demo.scene_configs[1].clone();
+        }
+    */
     let cfg = ctx.user_data.brush_demo.scene_configs[1].clone();
 
     // cfg.path_bounds.y.lower = oscillator::sine(ctx.frame_count, 0.01, 44, 64) as u32;
     // cfg.path_bounds.y.upper = cfg.path_bounds.y.lower;
 
-
-
     //**********************************************************************************************
-
 
     //
     //
@@ -285,14 +258,11 @@ pub fn render_frame(ctx: &mut GraphContext<DemoUserData>) {
             RectArea::new(0, 0, cell_width, cell_height, None),
             num_rows as usize,
             num_cols as usize,
-            Some(vec![ cfg.grid_colors.outer ]),
+            Some(vec![cfg.grid_colors.outer]),
         );
-
 
         grid.iter().enumerate().for_each(|(i, cell)| {
             let mut react_area = cell.rect_area();
-
-
 
             react_area.top_left =
                 react_area.top_left + Point::new(cfg.cell_deltas.outer, cfg.cell_deltas.outer);
